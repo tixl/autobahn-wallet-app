c25c453635455230ed3cc665b47e599afd5c1721
Valentin Kauth <valentinkauth@me.com>
2021-01-04 10:22:43 +0100

    Add swipe options using
    - Add ‘react-native-swipe-list-view’
    - Add round button component as hidden items

--- src/screens/WalletScreen.tsx
+++ src/screens/WalletScreen.tsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import styled from 'styled-components/native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { AssetCard, HeaderBar } from '../components';
+import { AssetCard, HeaderBar, iconName, RoundButton } from '../components';
 import { textSize, colors, spacing } from '../constants';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
 import { ExampleAsset, ExampleState } from '../redux/reducer/example';
 import { RootStackParamList } from '../navigation/stacks/RootStack';
+
+import { SwipeListView } from 'react-native-swipe-list-view';
 
 type Props = {
   children?: string;
@@ -27,11 +29,15 @@ export const WalletScreen: React.FC<Props> = (props) => {
     navigation.navigate('AssetDetail', { asset: asset });
   };
 
+  const handleButton = (type: string) => {
+    console.log('Button action ' + type + ' activated');
+  };
+
   return (
     <Container style={{ paddingTop: insets.top }}>
       <HeaderBar type="value" title="Assets" />
       <ContentContainer>
-        {assets.map((asset, index) => (
+        {/* {assets.map((asset, index) => (
           <AssetCard
             key={index}
             name={asset.name}
@@ -41,7 +47,51 @@ export const WalletScreen: React.FC<Props> = (props) => {
             amountUsd={asset.valueUsd.toString()}
             // disabled={asset.prefix == 'TXL'}
           />
-        ))}
+        ))} */}
+        <SwipeListView
+          style={{
+            paddingLeft: spacing.s,
+            paddingRight: spacing.s,
+            paddingTop: spacing.m,
+          }}
+          data={assets}
+          renderItem={(data, rowMap) => (
+            <AssetCard
+              key={data.index}
+              name={data.item.name}
+              prefix={data.item.prefix}
+              onPress={() => onButtonPress(data.item)}
+              amount={data.item.value.toString()}
+              amountUsd={data.item.valueUsd.toString()}
+              // disabled={asset.prefix == 'TXL'}
+            />
+          )}
+          renderHiddenItem={(data, rowMap) => (
+            <ButtonContainer key={data.index}>
+              <RoundButton
+                width={50}
+                title="Send"
+                icon={iconName.arrowLeft}
+                color={colors.LIGHT_BLUE}
+                onPress={() => handleButton('send')}
+              />
+              <RoundButton
+                width={50}
+                title="Receive"
+                icon={iconName.arrowRight}
+                color={colors.LIGHT_BLUE}
+                onPress={() => handleButton('receive')}
+              />
+            </ButtonContainer>
+          )}
+          closeOnRowOpen
+          closeOnRowBeginSwipe
+          leftOpenValue={50 + 2 * spacing.s}
+          rightOpenValue={-(50 + 2 * spacing.s)}
+          // leftActivationValue={20}
+          // onLeftAction={() => handleButton('send')}
+          // onRightAction={() => handleButton('receive')}
+        />
       </ContentContainer>
     </Container>
   );
@@ -51,9 +101,16 @@ const Container = styled.View`
   flex: 1;
 `;
 
-const ContentContainer = styled.ScrollView`
+const ContentContainer = styled.View`
   flex: 1;
-  padding: ${spacing.m}px ${spacing.s}px 0px;
+  /* padding: ${spacing.m}px ${spacing.s}px 0px; */
+`;
+
+const ButtonContainer = styled.View`
+  padding: ${spacing.s}px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const CenterText = styled.Text`
