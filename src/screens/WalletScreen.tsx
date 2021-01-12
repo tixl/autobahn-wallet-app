e44a275ff4f3bd1197f7f1d1a706a97626e9b024
Tammo <tammo@users.noreply.github.com>
2021-01-12 11:00:50 +0100

    wip: integrate sdk

--- src/screens/WalletScreen.tsx
+++ src/screens/WalletScreen.tsx
@@ -1,15 +1,28 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+// import { createAccountChain } from '@tixl/tixl-sdk-js/workflows/accountchain';
+// import { keySet } from '@tixl/tixl-sdk-js/workflows/api/keyset';
+
+import { crypto } from '../utils/crypto';
 
 export default function WalletScreen() {
+  const [accountChain, setAccountChain] = React.useState(null);
+
+  React.useEffect(() => {
+    (async () => {
+      // const keyset = await keySet(crypto);
+      // const ac = await createAccountChain(crypto, keyset);
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
       <ScrollView
         style={styles.container}
         contentContainerStyle={styles.contentContainer}
       >
-        <Text>Wallet</Text>
+        <Text>Wallet nice</Text>
       </ScrollView>
     </View>
   );
