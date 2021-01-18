8708905402fc1b9b3d771115ff3f0288e792b4d3
Valentin Kauth <valentinkauth@me.com>
2021-01-18 12:09:43 +0100

    Udpate login screen
    - Add empty mnemnic word components to fill in
    - Add temporary mnemonic checking

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import styled from 'styled-components/native';
 import { ScreenWrapper } from './wrapper/ScreenWrapper';
@@ -10,13 +10,46 @@ import { useDispatch, useSelector } from 'react-redux';
 
 type Props = {};
 
-const LoginScreen: React.FC<Props> = () => {
+const LoginScreen: React.FC<Props> = ({}) => {
   const navigation = useNavigation();
   const insets = useSafeAreaInsets();
 
-  // TODO: Create empty mnemonic phrase
+  const emptyMnemonic = Array(24).join('.').split('.');
+  const [enteredMnemonic, setEnteredMnemonic] = useState<string[]>(
+    emptyMnemonic
+  );
+  const [mnemonicValid, setMnemonicValid] = useState<boolean>(false);
 
-  const mnemonic: string[] = [];
+  // Add updated value to array of entered mnemonic phrase
+  const updateEnteredMnemomic = (index: number, value: string) => {
+    const updatedEnteredMnemonic = [...enteredMnemonic];
+    updatedEnteredMnemonic[index] = value;
+    setEnteredMnemonic(updatedEnteredMnemonic);
+  };
+
+  // Check if menmonic is valid
+  const validateMnemonic = (mnemonic: string[]) => {
+    // TODO
+    // (Check for size)
+    // Check for empty strings
+    return mnemonic.filter((word) => word == '').length <= 0;
+  };
+
+  const onUnlockPressed = async () => {
+    if (validateMnemonic(enteredMnemonic)) {
+      // const seed = await mnemonicToSeed(words);
+      // const bip32 = fromSeed(seed);
+      // navigation.navigate('LoginLoadingS', { phrase: bip32 });
+      console.log('Unlock button pressed');
+    }
+  };
+
+  // Validate entered mnemonic every time it was updated
+  useEffect(() => {
+    const isValid = validateMnemonic(enteredMnemonic);
+    console.log(isValid);
+    setMnemonicValid(isValid);
+  }, [enteredMnemonic]);
 
   return (
     <ScreenWrapper
@@ -24,18 +57,20 @@ const LoginScreen: React.FC<Props> = () => {
     >
       <Content style={{ paddingBottom: insets.bottom }}>
         <ScrollContainer>
-          <MnemonicPhrase mnemonic={mnemonic}></MnemonicPhrase>
+          <MnemonicPhrase
+            mnemonic={emptyMnemonic}
+            onValueUpdated={(index, value) =>
+              updateEnteredMnemomic(index, value)
+            }
+          ></MnemonicPhrase>
         </ScrollContainer>
         <BottomBar
-          onNext={() =>
-            navigation.navigate('MnemonicConfirm', { mnemonic: mnemonic })
-          }
-          nextButtonText="Next"
+          onNext={() => onUnlockPressed()}
+          nextButtonText="Unlock"
+          nextButtonDisabled={!mnemonicValid}
           onPrevious={() => navigation.goBack()}
           previousButtonText="Back"
-          text="Type your mnemonic phrase and we will recover your wallet
-
-          "
+          text="Type your mnemonic phrase and we will recover your wallet"
         />
       </Content>
     </ScreenWrapper>
