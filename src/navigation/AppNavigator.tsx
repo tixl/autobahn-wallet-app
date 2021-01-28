5ee05e5083d9b470f385b4f65c28c9a81c454040
Valentin Kauth <valentinkauth@me.com>
2021-01-28 15:02:08 +0100

    Add sdk functionality to app navigator

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { NativeModules } from 'react-native';
 // import Constants from 'expo-constants';
@@ -17,17 +17,14 @@ import LinkingConfiguration from './LinkingConfiguration';
 import { IntroStackScreen, RootStackScreen } from './stacks';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
-import { getAccountChain } from '@tixl/tixl-sdk-js/redux/chains/selectors';
-import { createReceiveTask } from '@tixl/tixl-sdk-js/redux/tasks/actions';
 import { getKeys } from '@tixl/tixl-sdk-js/redux/keys/selectors';
 import { useAccountChain } from '../hooks/useAccountChain';
-import { generateKeys } from '@tixl/tixl-sdk-js/redux/keys/actions';
 import {
-  createAccountChain,
+  reloadIndexedChains,
   updateBlockStatesNetwork,
 } from '@tixl/tixl-sdk-js/redux/chains/actions';
-import { getUnspent } from '@tixl/tixl-sdk-js/requests/getUnspent';
 import { useTaskRunner } from '../hooks/useTaskRunner';
+import { useScanHistory } from '../hooks/useScanHistory';
 
 const ProdChecker = NativeModules.ProdChecker;
 
@@ -36,56 +33,32 @@ const AppNavigator = () => {
   const keySet = useSelector(getKeys);
   const accountChain = useAccountChain();
   const state = useSelector((state: RootState) => state);
-  const receiveTasks = useSelector((state: RootState) => state.tasks.receive);
+  const scanHistory = useScanHistory();
 
   // Start task runner to scan for incoming tasks
   useTaskRunner();
 
-  // initially create the wallet keyset (this will later be handled inside the intro setup)
-  React.useEffect(() => {
-    (async () => {
-      if (keySet) return;
-
-      // dont call crypto immediately, usually these are user initiated anyways
-      await new Promise((resolve) => setTimeout(resolve, 100));
-
-      dispatch(generateKeys());
-    })();
-  }, []);
-
-  // create wallet by loading account chain (for keyset)
   React.useEffect(() => {
     if (!keySet) return;
-    if (accountChain) return;
-
-    // create account chain
-    dispatch(createAccountChain());
+    console.log('your AN address', keySet.sig.publicKey);
   }, [keySet]);
 
-  // update state for blocks
-  React.useEffect(() => {
-    if (!accountChain) return;
+  useEffect(
+    () => {
+      setTimeout(() => {
+        if (accountChain) {
+          dispatch(reloadIndexedChains());
+          dispatch(updateBlockStatesNetwork());
+        }
 
-    // lookup new block states
-    dispatch(updateBlockStatesNetwork());
-  }, [accountChain]);
-
-  // handle incoming send blocks (use task runner later)
-  React.useEffect(() => {
-    if (!keySet) return;
-
-    (async () => {
-      console.log('your AN address', keySet.sig.publicKey);
-
-      // find new blocks to receive
-      const res = await getUnspent(keySet.sig.publicKey);
-
-      // create receive tasks
-      res.blocks.forEach((send) => {
-        dispatch(createReceiveTask(send.signature, undefined, send.symbol));
-      });
-    })();
-  }, [keySet]);
+        scanHistory();
+      }, 250);
+    },
+    // eslint-disable-next-line
+    [
+      // do not add dependencies here
+    ]
+  );
 
   // Defindes is navigation tracking has been accepted (needs to be toogable in settings and will be asked on new device --> part of local storage)
   const trackingAccepted = false;
